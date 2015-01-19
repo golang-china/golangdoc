e395f107a0b8a8b4b30bc5e720075f095d68ac77
chai2010 <chaishushan@gmail.com>
2015-01-19 10:34:04 +0800

    fix appengine import path

--- appinit.go
+++ appinit.go
@@ -15,11 +15,11 @@ import (
 	"path"
 	"regexp"
 
-	"golang.org/x/tools/godoc"
-	"golang.org/x/tools/godoc/static"
-	"golang.org/x/tools/godoc/vfs"
-	"golang.org/x/tools/godoc/vfs/mapfs"
-	"golang.org/x/tools/godoc/vfs/zipfs"
+	"github.com/chai2010/golangdoc/godoc"
+	"github.com/chai2010/golangdoc/godoc/static"
+	"github.com/chai2010/golangdoc/godoc/vfs"
+	"github.com/chai2010/golangdoc/godoc/vfs/mapfs"
+	"github.com/chai2010/golangdoc/godoc/vfs/zipfs"
 )
 
 func init() {
